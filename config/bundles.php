8dbd74b2705b883e737701ffbc92b63195a6aa9d
Elena <zianielena@gmail.com>
2024-07-03 20:26:28 +0200

    authentication

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
